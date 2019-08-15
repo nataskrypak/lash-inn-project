fe352d27799877a2dcdd7efc0cb9a865f3ca58a2
nataskrypak <zakaznataiua@gmail.com>
2019-08-15 13:20:23 +0000

    sample code

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("book-online").addEventListener("click", bookOnline);
+
+function bookOnline() {
+    document.getElementsByTagName("book-online").innerHTML='https://dikidi.online/115105';
+    
+}
