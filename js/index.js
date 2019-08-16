a3d79922f7e2a3d543a157723fa2cfd7ac43af67
nataskrypak <zakaznataiua@gmail.com>
2019-08-16 13:33:57 +0000

    sample code

--- js/index.js
+++
@@ -1,5 +0,0 @@
-//document.getElementsByName('book-online').addEventListener("click", bookOnline);
-//function bookOnline() {
-//    document.getElementsByName('book-online').href = 'https://beauty.dikidi.net/uk/record/115105';
- //   return 'https://beauty.dikidi.net/uk/record/115105';
-//}
